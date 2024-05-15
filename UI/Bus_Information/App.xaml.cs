f8fbcf2744f09260d213163439182a5abb3591a6
도나스 <jaejoon4929@gmail.com>
2024-05-15 18:11:24 +0900

    UI 세팅

---
+++ UI/Bus_Information/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Bus_Information
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
